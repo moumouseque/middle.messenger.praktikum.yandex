6e9e9d332ba4488e9790bece07e4d2d931108ec3
Anton Kolotaev <lascivolin@gmail.com>
2022-07-21 21:27:04 +0300

    remove unused function

--- src/utils/register-event.js
+++ src/utils/register-event.js
@@ -4,10 +4,6 @@ export const registerEventListener = ({selector, eventType, handler}) => {
     events.push({selector, eventType, handler});
 }
 
-export const unregisterEventListener = () => {
-
-}
-
 export const registerEventHandler = () => {
     events.forEach(({selector, eventType, handler}) => {
         const node = document.querySelector(selector);

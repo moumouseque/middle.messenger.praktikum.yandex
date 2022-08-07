7854c584bbe8723ea3ff17cf057f7993d59ab17a
Anton Kolotaev <lascivolin@gmail.com>
2022-08-07 14:02:02 +0300

    add sprint_2

--- src/components/modal/modal.js
+++
@@ -1,5 +0,0 @@
-import template from './modal.hbs';
-
-import './modal.css';
-
-export default ({content, closeClassName, className}) => template({content, closeClassName, className});
--- src/pages/settings/components/avatar/avatar.js
+++
@@ -1,5 +0,0 @@
-import template from './avatar.hbs'
-
-import './avatar.css';
-
-export default () => template();
